7463c7a1c743b2156c734f3c356fac94278ac83e
Desperationis <fork_important@pm.me>
2021-03-05 12:09:54 -0500

    Restructure site with template build system and new pages
    Add a Python build system (build.py) that assembles pages from
    shared partials (_partials/) and source templates (_src/) using
    placeholder substitution. Introduces config.json for site-wide
    settings, a lab registry for auto-generated lab cards, and new
    About and Videos pages. Overhauls CSS with theme variables, dark
    mode support, and responsive layout. Removes unused legacy files
    (markdown_report, md_make.py, old images).

---
+++ README.md
@@ -0,0 +1,64 @@
+# RSS Team Website Template
+
+This is a generic template for your team website hosted on github pages.
+
+## Modifying the template
+
+You can change the template design as much as you want - this website is pretty boring right now. Use pictures, videos, gifs and stories from your RSS experience to make this website pop! There are a couple constraints however. First, your lab reports must remain accessible at these links:
+
+    https://rss2026-[TEAM_NUMBER].github.io/website/labs/[LAB_NUMBER]
+
+Also for the sake of the graders, please make sure that the text remains **black on white**.
+
+And finally **don't add too much javascript/bloat**. We're going to need to go through a whole bunch of these websites rapidly and we don't want it to be too slow to load.
+
+## Setting up the template
+
+You will be hosting this template using [github pages](https://pages.github.com/).
+If you haven't already done so, have your team make an organization on [github.com](https://github.com). Make sure your organization is named ```rss2026-#``` where "#" is your team number (not your car number!). Do not pad the number with zeros.
+
+Then fork this repo into your organization, and rename the repo to just `website`. In the settings page of the forked repo scroll down to the "Github Pages" section. Under "Source", click the dropdown menu and select "master branch" then hit save. You should now be able to see the template if you navigate to:
+
+    https://rss2026-[TEAM_NUMBER].github.io/website/
+
+Now in order to modify the template, clone your fork and read on to the next section.
+
+## Using the template for lab reports
+
+**You must follow the instructions given in `/latex_report/latex_report.pdf` for your lab reports. This document outlines the expectations for the reports as well as an outline of the required sections.**
+
+The template has a home page written in html at `/index.html`. It also has a labs page at `/labs/index.html`. You will add folders for each of your labs inside the `/labs` folder, and each one will need to have a corresponding `index.html` file as well as any supporting files (images, etc.). You can take a look at the `/labs/example_lab` section. There are several options for how to generate your lab report as a PDF, but at minimum you must provide links to your briefing slides and a download of your report in PDF format, as well as embedding the slides and report as shown in `/labs/example_lab/index.html`. You can see how this example lab is rendered at 
+https://mit-rss.github.io/website2022/labs/example_lab/
+
+**To embed your slideshow onto the page** as is shown in the link above, go on your Google Slides page, then click File > Publish to the Web. Then, select the Embed tab. After selecting the options you'd like for the slides, it should give you HTML code which can be pasted into your HTML file to add your slides to the page.
+
+One simple way to generate this PDF would be to use a LaTeX tool such as [Overleaf](https://www.overleaf.com/) (think google docs for LaTeX) to collaborate with your team. You can use the file `/latex_report/latex_report.tex` as a starter template - it was used to produce the lab report instructions at `/latex_report/latex_report.pdf` if you want to see how the corresponding LaTeX code renders in a PDF. It also has examples of how to create elements like tables, figures, pseudocode, and code blocks in LaTeX.
+
+To view local changes that are made to your website repo on github pages, commit them and refresh the page. You might need to do a hard refresh depending on your browser / cookie settings.
+
+## Optional
+
+If you would like more creative freedom, you are also welcome to render your report directly in HTML in addition to your PDF.
+
+Another option is to write your report in markdown and then run the provided `md_make.py` script, which will generate an `html.txt` file with your report translated into HTML. To render this on your website, paste the contents of `html.txt` into your `index.html` file (see `/labs/example_lab/index.html` for where to paste). You will have to edit `md_make.py` to read from the correct filename where you have your markdown. **Again, note that these methods are optional, but providing a download link and embedding a PDF of your report is required.**
+
+If you do want to use `md_make.py`, you should first install mistune:
+```
+    sudo apt-get install python3-pip
+    sudo pip3 install mistune
+```
+
+If you want to make your report fancier you can also modify md_mark.py with [custom options](http://www.discoversdk.com/blog/custom-markdown-parsing-with-mistune-and-python), and if you want to generate a pdf from markdown you can install pandoc and run
+```
+    pandoc lab_report.md -o lab_report.pdf
+```
+
+The website template also supports $\LaTeX$ via mathjax (this is a script at the beginning of the html lab files)! For inline math use single dollar signs and for multiline math use use double dollar signs like this:
+
+$$
+\begin{align}
+  e^{i\pi} + 1 = 0
+\end{align}
+$$
+
+If you want to use markdown, you can use the file `markdown_report/markdown_report.md` as a starter template - it has examples of headers and text formatting as well as how to create elements like tables, figures, and code blocks in markdown.
---
+++ build.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+"""build.py — Assembles the RSS Team 7 website from templates and partials.
+
+Usage:
+    python3 build.py          Build all pages
+    python3 build.py --clean  Remove generated output files
+
+How to update the site:
+    1. Edit files in _src/ and _partials/ (NOT the root HTML files)
+    2. Run: python3 build.py
+    3. Commit all changes and push to GitHub
+
+How to add a new team member:
+    1. Add their entry to _src/config.json under "members"
+    2. Put their photo in img/ (or leave "photo" empty for placeholder)
+    3. Run: python3 build.py
+
+How to add a new lab:
+    1. Copy _src/labs/_template/ to _src/labs/[LAB_NUMBER]/
+    2. Edit _src/labs/[LAB_NUMBER]/index.html (follow the EDIT THIS comments)
+    3. Add an entry to _src/labs/_registry.json
+    4. Run: python3 build.py
+"""
+
+import json
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARTIALS_DIR = os.path.join(ROOT_DIR, '_partials')
+SRC_DIR = os.path.join(ROOT_DIR, '_src')
+CONFIG_PATH = os.path.join(SRC_DIR, 'config.json')
+REGISTRY_PATH = os.path.join(SRC_DIR, 'labs', '_registry.json')
+
+# Static pages: (source_path relative to _src, output_path relative to root, depth)
+PAGES = [
+    ('index.html', 'index.html', 0),
+    ('about.html', 'about/index.html', 1),
+    ('videos.html', 'videos/index.html', 1),
+    ('labs/index.html', 'labs/index.html', 1),
+]
+
+
+def load_partials():
+    """Load all HTML partials from _partials/ directory."""
+    partials = {}
+    for entry in sorted(os.listdir(PARTIALS_DIR)):
+        if not entry.endswith('.html'):
+            continue
+        name = entry[:-5].upper().replace('-', '_')
+        with open(os.path.join(PARTIALS_DIR, entry), 'r') as f:
+            partials[name] = f.read()
+    return partials
+
+
+def load_config():
+    """Load site-wide configuration from config.json."""
+    with open(CONFIG_PATH, 'r') as f:
+        return json.load(f)
+
+
+def load_registry():
+    """Load lab metadata from _registry.json."""
+    with open(REGISTRY_PATH, 'r') as f:
+        return json.load(f)
+
+
+def root_path(depth):
+    """Return the correct relative root path for a given depth."""
+    if depth == 0:
+        return '.'
+    return '/'.join(['..'] * depth)
+
+
+def generate_team_cards(members, root):
+    """Generate HTML for team member cards."""
+    cards = []
+    for member in members:
+        if member.get('photo'):
+            img = (f'<img class="headshot" src="{root}/{member["photo"]}" '
+                   f'alt="Photo of {member["name"]}">')
+        else:
+            img = (f'<div class="placeholder-headshot" role="img" '
+                   f'aria-label="Placeholder photo for {member["name"]}">'
+                   f'<i class="fa fa-user"></i></div>')
+        card = (
+            f'    <div class="team-card">\n'
+            f'      {img}\n'
+            f'      <h4>{member["name"]}</h4>\n'
+            f'      <p class="email">{member["email"]}</p>\n'
+            f'      <p class="bio">{member["bio"]}</p>\n'
+            f'    </div>'
+        )
+        cards.append(card)
+    return '\n'.join(cards)
+
+
+def generate_latest_lab(labs, root):
+    """Generate HTML for the latest lab highlight, or a 'Coming soon' placeholder."""
+    if not labs:
+        return '  <p class="coming-soon" style="color:var(--text-muted);">Coming soon — check back after our first lab!</p>'
+    lab = labs[-1]
+    thumbnail = lab.get('thumbnail', '')
+    if thumbnail:
+        img = f'<img src="{root}/{thumbnail}" alt="{lab["title"]} thumbnail">'
+    else:
+        img = (f'<div class="placeholder-lab-img" '
+               f'data-lab-number="{lab["number"]}"></div>')
+    return (
+        f'  <div class="labs-grid" style="max-width:480px; margin:0 auto;">\n'
+        f'    <div class="lab-card">\n'
+        f'      {img}\n'
+        f'      <div class="lab-card-body">\n'
+        f'        <h3>{lab["title"]}</h3>\n'
+        f'        <p>{lab["description"]}</p>\n'
+        f'        <a href="{root}/labs/{lab["number"]}/" class="btn">'
+        f'<i class="fa fa-info-circle"></i> More Info</a>\n'
+        f'      </div>\n'
+        f'    </div>\n'
+        f'  </div>'
+    )
+
+
+def generate_lab_cards(labs):
+    """Generate HTML for lab cards from registry."""
+    cards = []
+    for lab in labs:
+        thumbnail = lab.get('thumbnail', '')
+        if thumbnail:
+            img = f'<img src="{thumbnail}" alt="{lab["title"]} thumbnail">'
+        else:
+            img = (f'<div class="placeholder-lab-img" '
+                   f'data-lab-number="{lab["number"]}"></div>')
+        card = (
+            f'    <!-- {lab["title"]} -->\n'
+            f'    <div class="lab-card">\n'
+            f'      {img}\n'
+            f'      <div class="lab-card-body">\n'
+            f'        <h3>{lab["title"]}</h3>\n'
+            f'        <p>{lab["description"]}</p>\n'
+            f'        <a href="{lab["number"]}" class="btn">'
+            f'<i class="fa fa-info-circle"></i> More Info</a>\n'
+            f'      </div>\n'
+            f'    </div>'
+        )
+        cards.append(card)
+    return '\n'.join(cards)
+
+
+def discover_lab_pages():
+    """Auto-discover lab pages from _src/labs/*/index.html, excluding _template/."""
+    lab_pages = []
+    labs_src = os.path.join(SRC_DIR, 'labs')
+    if not os.path.isdir(labs_src):
+        return lab_pages
+    for entry in sorted(os.listdir(labs_src)):
+        if entry.startswith('_') or entry.startswith('.'):
+            continue
+        lab_index = os.path.join(labs_src, entry, 'index.html')
+        if os.path.isfile(lab_index):
+            lab_pages.append((
+                f'labs/{entry}/index.html',
+                f'labs/{entry}/index.html',
+                2
+            ))
+    return lab_pages
+
+
+def build_page(source_path, output_path, depth, partials, config, registry):
+    """Build a single page from its source template."""
+    src_file = os.path.join(SRC_DIR, source_path)
+    out_file = os.path.join(ROOT_DIR, output_path)
+
+    with open(src_file, 'r') as f:
+        content = f.read()
+
+    root = root_path(depth)
+
+    # Replace partials
+    for name, partial_content in partials.items():
+        content = content.replace('{{' + name + '}}', partial_content)
+
+    # Replace config variables
+    replacements = {
+        '{{TEAM_NAME}}': config['team_name'],
+        '{{GITHUB_URL}}': config['github_url'],
+        '{{COURSE}}': config['course'],
+        '{{SEMESTER}}': config['semester'],
+        '{{HERO_IMAGE}}': config.get('hero_image', 'img/mit.jpg'),
+        '{{TAGLINE}}': config.get('tagline', ''),
+    }
+    for placeholder, value in replacements.items():
+        content = content.replace(placeholder, value)
+
+    # Replace auto-generated content
+    content = content.replace('{{TEAM_CARDS}}', generate_team_cards(config['members'], root))
+    content = content.replace('{{LAB_CARDS}}', generate_lab_cards(registry))
+    content = content.replace('{{LATEST_LAB}}', generate_latest_lab(registry, root))
+
+    # Replace {{ROOT}} last (after partials and team cards that contain {{ROOT}})
+    content = content.replace('{{ROOT}}', root)
+
+    # Create output directory
+    os.makedirs(os.path.dirname(out_file), exist_ok=True)
+
+    with open(out_file, 'w') as f:
+        f.write(content)
+
+    return out_file
+
+
+def clean():
+    """Remove generated output files."""
+    lab_pages = discover_lab_pages()
+    all_pages = PAGES + lab_pages
+    removed = []
+    for _, output_path, _ in all_pages:
+        out_file = os.path.join(ROOT_DIR, output_path)
+        if os.path.exists(out_file):
+            os.remove(out_file)
+            removed.append(output_path)
+    # Clean empty directories created by build
+    for dirpath in ['about', 'videos']:
+        d = os.path.join(ROOT_DIR, dirpath)
+        if os.path.isdir(d) and not os.listdir(d):
+            os.rmdir(d)
+            removed.append(f'{dirpath}/')
+    print(f'Cleaned {len(removed)} files')
+    for f in removed:
+        print(f'  removed {f}')
+
+
+def main():
+    partials = load_partials()
+    config = load_config()
+    registry = load_registry()
+    lab_pages = discover_lab_pages()
+
+    if '--clean' in sys.argv:
+        clean()
+        return
+
+    all_pages = PAGES + lab_pages
+    built = []
+
+    for source_path, output_path, depth in all_pages:
+        src_file = os.path.join(SRC_DIR, source_path)
+        if not os.path.exists(src_file):
+            print(f'  SKIP {output_path} (source not found: _src/{source_path})')
+            continue
+        build_page(source_path, output_path, depth, partials, config, registry)
+        built.append(output_path)
+
+    print(f'Built {len(built)} pages:')
+    for f in built:
+        print(f'  {f}')
+
+
+if __name__ == '__main__':
+    main()
